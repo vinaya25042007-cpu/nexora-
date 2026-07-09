b096d6c92eac9cf058781936d842f9b98cb5a954
radhahari2007-stack <radhahari2007@gmail.com>
2026-07-10 02:19:37 +0530

    Add files via upload

--- exceptions.py
+++ exceptions.py
@@ -1,176 +1,59 @@
-from typing import Any, Dict, Optional, Sequence, Type, Union
+'''
+Custom exceptions raised by pytz.
+'''
 
-from pydantic import BaseModel, create_model
-from starlette.exceptions import HTTPException as StarletteHTTPException
-from starlette.exceptions import WebSocketException as StarletteWebSocketException
-from typing_extensions import Annotated, Doc
-
-
-class HTTPException(StarletteHTTPException):
-    """
-    An HTTP exception you can raise in your own code to show errors to the client.
-
-    This is for client errors, invalid authentication, invalid data, etc. Not for server
-    errors in your code.
-
-    Read more about it in the
-    [FastAPI docs for Handling Errors](https://fastapi.tiangolo.com/tutorial/handling-errors/).
-
-    ## Example
-
-    ```python
-    from fastapi import FastAPI, HTTPException
-
-    app = FastAPI()
-
-    items = {"foo": "The Foo Wrestlers"}
+__all__ = [
+    'UnknownTimeZoneError', 'InvalidTimeError', 'AmbiguousTimeError',
+    'NonExistentTimeError',
+]
 
 
-    @app.get("/items/{item_id}")
-    async def read_item(item_id: str):
-        if item_id not in items:
-            raise HTTPException(status_code=404, detail="Item not found")
-        return {"item": items[item_id]}
-    ```
-    """
-
-    def __init__(
-        self,
-        status_code: Annotated[
-            int,
-            Doc(
-                """
-                HTTP status code to send to the client.
-                """
-            ),
-        ],
-        detail: Annotated[
-            Any,
-            Doc(
-                """
-                Any data to be sent to the client in the `detail` key of the JSON
-                response.
-                """
-            ),
-        ] = None,
-        headers: Annotated[
-            Optional[Dict[str, str]],
-            Doc(
-                """
-                Any headers to send to the client in the response.
-                """
-            ),
-        ] = None,
-    ) -> None:
-        super().__init__(status_code=status_code, detail=detail, headers=headers)
+class Error(Exception):
+    '''Base class for all exceptions raised by the pytz library'''
 
 
-class WebSocketException(StarletteWebSocketException):
-    """
-    A WebSocket exception you can raise in your own code to show errors to the client.
+class UnknownTimeZoneError(KeyError, Error):
+    '''Exception raised when pytz is passed an unknown timezone.
 
-    This is for client errors, invalid authentication, invalid data, etc. Not for server
-    errors in your code.
+    >>> isinstance(UnknownTimeZoneError(), LookupError)
+    True
 
-    Read more about it in the
-    [FastAPI docs for WebSockets](https://fastapi.tiangolo.com/advanced/websockets/).
+    This class is actually a subclass of KeyError to provide backwards
+    compatibility with code relying on the undocumented behavior of earlier
+    pytz releases.
 
-    ## Example
+    >>> isinstance(UnknownTimeZoneError(), KeyError)
+    True
 
-    ```python
-    from typing import Annotated
+    And also a subclass of pytz.exceptions.Error, as are other pytz
+    exceptions.
 
-    from fastapi import (
-        Cookie,
-        FastAPI,
-        WebSocket,
-        WebSocketException,
-        status,
-    )
+    >>> isinstance(UnknownTimeZoneError(), Error)
+    True
 
-    app = FastAPI()
-
-    @app.websocket("/items/{item_id}/ws")
-    async def websocket_endpoint(
-        *,
-        websocket: WebSocket,
-        session: Annotated[str | None, Cookie()] = None,
-        item_id: str,
-    ):
-        if session is None:
-            raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-        await websocket.accept()
-        while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Session cookie is: {session}")
-            await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
-    ```
-    """
-
-    def __init__(
-        self,
-        code: Annotated[
-            int,
-            Doc(
-                """
-                A closing code from the
-                [valid codes defined in the specification](https://datatracker.ietf.org/doc/html/rfc6455#section-7.4.1).
-                """
-            ),
-        ],
-        reason: Annotated[
-            Union[str, None],
-            Doc(
-                """
-                The reason to close the WebSocket connection.
-
-                It is UTF-8-encoded data. The interpretation of the reason is up to the
-                application, it is not specified by the WebSocket specification.
-
-                It could contain text that could be human-readable or interpretable
-                by the client code, etc.
-                """
-            ),
-        ] = None,
-    ) -> None:
-        super().__init__(code=code, reason=reason)
-
-
-RequestErrorModel: Type[BaseModel] = create_model("Request")
-WebSocketErrorModel: Type[BaseModel] = create_model("WebSocket")
-
-
-class FastAPIError(RuntimeError):
-    """
-    A generic, FastAPI-specific error.
-    """
-
-
-class ValidationException(Exception):
-    def __init__(self, errors: Sequence[Any]) -> None:
-        self._errors = errors
-
-    def errors(self) -> Sequence[Any]:
-        return self._errors
-
-
-class RequestValidationError(ValidationException):
-    def __init__(self, errors: Sequence[Any], *, body: Any = None) -> None:
-        super().__init__(errors)
-        self.body = body
-
-
-class WebSocketRequestValidationError(ValidationException):
+    '''
     pass
 
 
-class ResponseValidationError(ValidationException):
-    def __init__(self, errors: Sequence[Any], *, body: Any = None) -> None:
-        super().__init__(errors)
-        self.body = body
+class InvalidTimeError(Error):
+    '''Base class for invalid time exceptions.'''
 
-    def __str__(self) -> str:
-        message = f"{len(self._errors)} validation errors:\n"
-        for err in self._errors:
-            message += f"  {err}\n"
-        return message
+
+class AmbiguousTimeError(InvalidTimeError):
+    '''Exception raised when attempting to create an ambiguous wallclock time.
+
+    At the end of a DST transition period, a particular wallclock time will
+    occur twice (once before the clocks are set back, once after). Both
+    possibilities may be correct, unless further information is supplied.
+
+    See DstTzInfo.normalize() for more info
+    '''
+
+
+class NonExistentTimeError(InvalidTimeError):
+    '''Exception raised when attempting to create a wallclock time that
+    cannot exist.
+
+    At the start of a DST transition period, the wallclock time jumps forward.
+    The instants jumped over never occur.
+    '''
